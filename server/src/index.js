4fe0a573f05649b89b605bff0afef70ce7f3da56
mohammadreza memar <moreza.memar@gmail.com>
2023-03-29 16:13:32 -0500

    user connected to database

--- server/src/index.js
+++ server/src/index.js
@@ -1,1 +1,24 @@
 import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import { userRouter } from "./routes/users.js";
+// // import { recipesRouter } from "./routes/recipes.js";
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.use("/auth", userRouter);
+// // app.use("/recipes", recipesRouter);
+
+mongoose.connect(
+  "mongodb+srv://mohammad:PaxYywshnQYydE9u@recipez.eryllca.mongodb.net/recipez?retryWrites=true&w=majority",
+
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
+
+app.listen(3001, () => console.log("Server started"));
