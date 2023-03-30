367abb21de02c471d2767f1cf08a4895496c896d
mohammadreza memar <moreza.memar@gmail.com>
2023-03-30 12:55:20 -0500

    fixed the navbar

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -1,6 +1,6 @@
-import { Link } from "react-router-dom";
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
 import { useCookies } from "react-cookie";
-import { useNavigate } from "react-router-dom";
 
 export const Navbar = () => {
   const [cookies, setCookies] = useCookies(["access_token"]);
@@ -8,7 +8,7 @@ export const Navbar = () => {
 
   const logout = () => {
     setCookies("access_token", "");
-    window.localStorage.removeItem("userID");
+    window.localStorage.clear();
     navigate("/auth");
   };
   return (
