c49175f51ae88268435b517217a51084d4644747
mohammadreza memar <moreza.memar@gmail.com>
2023-03-30 12:46:35 -0500

    verifyToken middleware is added to user

--- server/src/routes/users.js
+++ server/src/routes/users.js
@@ -1,42 +1,54 @@
 import express from "express";
 import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
-import { UserModel } from "../models/Users.js";
 
 const router = express.Router();
+import { UserModel } from "../models/Users.js";
 
 router.post("/register", async (req, res) => {
   const { username, password } = req.body;
   const user = await UserModel.findOne({ username });
-
   if (user) {
-    return res.json({ message: "User Already Exists!!" });
+    return res.status(400).json({ message: "Username already exists" });
   }
-
   const hashedPassword = await bcrypt.hash(password, 10);
-
   const newUser = new UserModel({ username, password: hashedPassword });
   await newUser.save();
-
-  res.json({ message: "User Registered Successfully" });
+  res.json({ message: "User registered successfully" });
 });
 
 router.post("/login", async (req, res) => {
   const { username, password } = req.body;
+
   const user = await UserModel.findOne({ username });
 
   if (!user) {
-    return res.json({ message: "User Doesn't Exist!" });
+    return res
+      .status(400)
+      .json({ message: "Username or password is incorrect" });
   }
-
   const isPasswordValid = await bcrypt.compare(password, user.password);
-
   if (!isPasswordValid) {
-    return res.json({ message: "Username or Password Is Incorrect!" });
+    return res
+      .status(400)
+      .json({ message: "Username or password is incorrect" });
   }
-
   const token = jwt.sign({ id: user._id }, "secret");
   res.json({ token, userID: user._id });
 });
 
 export { router as userRouter };
+
+export const verifyToken = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  if (authHeader) {
+    jwt.verify(authHeader, "secret", (err) => {
+      if (err) {
+        return res.sendStatus(403);
+      }
+      next();
+    });
+  } else {
+    res.sendStatus(401);
+  }
+};
