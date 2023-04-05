5004b850aee99152b8b1a55214f028b2e9e5fc04
mohammadreza memar <moreza.memar@gmail.com>
2023-04-05 12:44:52 -0500

    fixed the error

--- server/src/index.js
+++ server/src/index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
-import { userRouter } from "./routes/users.js";
+import { userRouter } from "./routes/user.js";
 import { recipesRouter } from "./routes/recipes.js";
 
 const app = express();
